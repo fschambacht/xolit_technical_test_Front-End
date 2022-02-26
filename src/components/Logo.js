4ba072db811a5cd4938d503d1b20fc545bcb22b8
fschambacht <aschambacht@gmail.com>
2022-02-25 22:54:41 -0500

    [CREATE] component Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import './logo.css'
+
+const Logo = ({props}) => {
+  return (
+    <div>
+      <img
+      className='img'
+      src='https://img.icons8.com/fluency/96/000000/online-shop-card-payment.png'
+      />
+    </div>
+  )
+}
+
+export default Logo

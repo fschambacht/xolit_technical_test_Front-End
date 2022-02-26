a048ea382daba4614766e31b7001d3620cf0062d
fschambacht <aschambacht@gmail.com>
2022-02-25 22:55:23 -0500

    [CREATE] component Carro

---
+++ src/components/Carro.js
@@ -0,0 +1,13 @@
+import './carro.css'
+
+const Carro = () => {
+  return (
+    <div className='carro'>
+      <img
+        src='https://img.icons8.com/external-phatplus-lineal-color-phatplus/64/000000/external-shopping-cyber-monday-phatplus-lineal-color-phatplus.png'
+      />
+    </div>
+  )
+}
+
+export default Carro

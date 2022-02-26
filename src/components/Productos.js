e1541fa6b287fbaa322ac6d858d7b396789a1dc1
fschambacht <aschambacht@gmail.com>
2022-02-25 22:52:27 -0500

    [CREATE] component Productos

---
+++ src/components/Productos.js
@@ -0,0 +1,13 @@
+import './productos.css'
+import Producto from './Producto'
+
+const Productos = () => {
+  return (
+    <div className='productos'>
+      <Producto>
+      </Producto>
+    </div>
+  )
+}
+
+export default Productos

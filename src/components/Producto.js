1e19477ec68f2d08e1d2cf68d854df817adc1ace
fschambacht <aschambacht@gmail.com>
2022-02-26 18:25:56 -0500

    [MODIFY] add obj count, add arr function addProductos, change URL to fetch in deploy back-end, create arr function factura to render products selected

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -4,25 +4,42 @@ import './producto.css'
 
 const Producto = () => {
   const [producto, setProducto] = useState([])
+
   const [productoAgregado, setProductoAgregado] = useState([])
+  let count = {count: 1}
+
+  const addProductos = (prop) => {
+    
+    productoAgregado.map(objeto => objeto.id === prop.id ? count.count += 1 : null)
+
+    const newProd = {...prop, ...count}
+    
+    setProductoAgregado([
+      ...productoAgregado,
+      newProd
+    ])
+  }
 
   const getProductos = async () => {
-    const peticion = await fetch('http://127.0.0.1:8000/products')
+    const peticion = await fetch('https://technical-test-xolit.herokuapp.com/products')
     const respuesta = await peticion.json()
     return setProducto(respuesta)
   }
 
-  const addProductos = (prop) => {
-    productoAgregado.map(objeto => objeto.id === prop.id ? prop.count += 1 : null)
+  const factura = (arrayOriginal, propiedad) => {
+    const nuevoArray = []
+    const objetoBuscado = {}
 
-    setProductoAgregado([
-      //...productoAgregado,
-      prop,
-    ])
+    for(const obj in arrayOriginal) {
+      objetoBuscado[arrayOriginal[obj][propiedad]] = arrayOriginal[obj]
+    }
 
-    console.log(productoAgregado);
+    for(const i in objetoBuscado) {
+      nuevoArray.push(objetoBuscado[i])
+    }
+    return nuevoArray
   }
-
+  
   return (
     <div onClick={getProductos} >
       {producto.length == 0
@@ -34,7 +51,7 @@ const Producto = () => {
       ></Tarjeta>
       <ul>
         { 
-          productoAgregado.map(prod => prod.id ===  <li key={prod.count}>{prod.nombre}---{prod.count}</li>
+          factura(productoAgregado,'id').map(prod => <li key={prod.id}>{prod.nombre}---{prod.count}</li>
         )
         }
       </ul>

e821ef06ab3d4fa933937c138e219526ee6e9a94
fschambacht <aschambacht@gmail.com>
2022-02-26 18:24:06 -0500

    [MODIFY] move obj count and modify event onClick

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,41 +1,38 @@
-import {useState} from 'react'
 import './tarjeta.css'
 
-const Tarjeta = ({ producto, addProductos }) => {
-  let count = {count: 0}
-
-  return (
-    <div className='tarjeta'>
-      { producto.map(prop => {
-        return (
-          <div 
-            key={prop.id}
-            className='producto'
+const Tarjeta = ({ producto, addProductos, count }) => {
+  return(
+  <div className='tarjeta'>
+    { producto.map(prop => {
+      return (
+        <div 
+          key={prop.id}
+          className='producto'
+        >
+          <span>
+            <label>Nombre</label>
+            <h3>{prop.nombre}</h3>
+          </span>
+          <span>
+            <label>Costo</label>
+            <p>{prop.valorVentaConIva}</p>
+          </span>
+          <span>
+            <label>un disp</label>
+            <p>{prop.cantidadUnidadesInventario}</p>
+          </span>
+          <button
+            onClick={() => {
+              addProductos(prop)
+            }}
+            type='button'
+            children='Seleccionar'
           >
-            <span>
-              <label>Nombre</label>
-              <h3>{prop.nombre}</h3>
-            </span>
-            <span>
-              <label>Costo</label>
-              <p>{prop.valorVentaConIva}</p>
-            </span>
-            <span>
-              <label>un disp</label>
-              <p>{prop.cantidadUnidadesInventario}</p>
-            </span>
-            <button
-              onClick={() => {
-                addProductos({...count, ...prop})
-              }}
-              type='button'
-              children='Seleccionar'
-            >
-            </button>
-          </div>
-        )})
-      }
-    </div>
+          </button>
+        </div>
+      )})
+    }
+  </div>
   )
 }
 

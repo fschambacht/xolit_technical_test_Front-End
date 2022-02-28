c16a5d52beb4a11f277d9b2aa7cd10755e274ac7
fschambacht <aschambacht@gmail.com>
2022-02-27 23:02:52 -0500

    [CREATE] component to handle inputs and his props

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import './Input.css'
+
+export const Input = ({ label, ...rest }) => {
+  return(
+    <div className='field'>
+      <label>{label}</label>
+      <input {...rest}/>
+    </div>
+  )
+}
+

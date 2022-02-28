ce9bf837fe69fa2500b274add8eda719b2fa4287
fschambacht <aschambacht@gmail.com>
2022-02-27 23:19:09 -0500

    [MODIFY] add prop alt to an image

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,6 +4,7 @@ const Logo = ({props}) => {
   return (
     <div>
       <img
+      alt='imagen de una tienda como logo'
       className='img'
       src='https://img.icons8.com/fluency/96/000000/online-shop-card-payment.png'
       />

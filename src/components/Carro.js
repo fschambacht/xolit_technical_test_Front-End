d2f372df4a0588f87abab3d8a3c0012e86ac51ac
fschambacht <aschambacht@gmail.com>
2022-02-27 23:07:21 -0500

    [CREATE] component Carro to handle methods POST and render form

--- src/components/Carro.js
+++ src/components/Carro.js
@@ -1,11 +1,49 @@
+import { useState } from 'react'
 import './carro.css'
+import Formulario from './Formulario'
 
-const Carro = () => {
+const Carro = ({productoAgregado, factura}) => {
+  const [renderFormulario, setRenderFormulario ] = useState(false)
+  
+  const enviarProductos = async () => {
+    
+    const seleccion = factura(productoAgregado, 'id')
+
+    await fetch('https://technical-test-xolit.herokuapp.com/add/products', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify(seleccion)
+    })
+
+    setRenderFormulario(true)
+
+    console.log(seleccion)
+  }
+  
   return (
-    <div className='carro'>
-      <img
-        src='https://img.icons8.com/external-phatplus-lineal-color-phatplus/64/000000/external-shopping-cyber-monday-phatplus-lineal-color-phatplus.png'
-      />
+    <div>
+      <div 
+        className='carro'
+        onClick={enviarProductos}
+      >
+        <label>Fin Agregar Productos</label>
+        <img
+          alt='imagen de carrrito de compra'
+          className='img'
+          src='https://img.icons8.com/external-phatplus-lineal-color-phatplus/64/000000/external-shopping-cyber-monday-phatplus-lineal-color-phatplus.png'
+        />
+      </div>
+      <div>
+        {
+          renderFormulario 
+          ? <Formulario
+
+          /> 
+          : null
+        }
+      </div>
     </div>
   )
 }
